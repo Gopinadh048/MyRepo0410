1bc63512bc10bc8e920e16c7a7b768a1fe124c71
tnrao-trainer <40280456+tnrao-trainer@users.noreply.github.com>
2021-10-04 19:35:23 +0530

    my latest update in remote

--- index.js
+++ index.js
@@ -29,4 +29,5 @@ app.get('/',function(req,res)
 var server=app.listen(3002,function() {});
  
 console.log("Updated in local repository");
+console.log("Updated in remote repository");
 console.log("Server Started. URL:http://localhost:3002");
